5dfd11cf94c9ec0e6f75209c4063323b785405e8
blackdayalpha <93610550+blackdayalpha@users.noreply.github.com>
2024-02-19 20:21:27 +0530

    Update DisplayArea.js

--- DisplayArea.js
+++ DisplayArea.js
@@ -3,23 +3,42 @@ Ext.define('ExtractApp.view.main.DisplayArea', {
     xtype: 'app-displayArea',
     alias: 'view.displayArea',
 
-    controller: 'main',
-    viewModel: 'main',
-    layout: 'hbox',
-    items: [{
-        xtype: 'displayfield',
-        bind: {
-            value: '{displayExtractedData}',
+    // controller: 'main',
+    // viewModel: 'main',
+    id: 'id-display-content',
+    alert: console.log('Display Area created!'),
+    items: [
+        {
+            layout: 'hbox',
+            id: 'display-area',
+            items: [{
+                xtype: 'panel',
+                emptyText: 'hello',
+                title: 'Formatted Data',
+                flex: 1,
+                bind: { html: '{getFormattedOutputFromJson}' },
+                cls: 'display-item',
+                height: 350,
+                scrollable: true
+            },
+            {
+                xtype: 'panel',
+                // bind: '{JData}',
+                title: 'JSON',
+                bind: {
+                    html: '{JData}'
+                },
+                flex: 1,
+                height: 350,
+                cls: 'display-item',
+                scrollable: true,
+            }]
         },
-        emptyText: 'hello',
-        flex: 1
-    },
-    {
-        xtype: 'displayfield',
-        bind: {
-            value: '{jsonData}',
-        },
-        flex: 1
-    }
+        {
+            xtype: 'button',
+            id: 'reset-btn',
+            text: 'Reset',
+            handler: 'ResetView',
+        }
     ]
 });
