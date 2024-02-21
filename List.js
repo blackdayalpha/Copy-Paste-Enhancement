2e7388456192f2ca39b469d2a18cd1965a243468
blackdayalpha <93610550+blackdayalpha@users.noreply.github.com>
2024-02-21 10:56:59 +0530

    Update List.js

--- List.js
+++ List.js
@@ -8,182 +8,38 @@ Ext.define('ExtractApp.view.main.List', {
     requires: [
         'ExtractApp.store.Personnel'
     ],
+    margin: '0 0 15 0',
+    padding: '10 10 10 10',
+    border: true,
 
-    title: 'Personnel',
-
+    markDirty: false,
     store: {
         type: 'personnel'
     },
 
-    columns: [
-        {
-            text: 'Field Name',
-            width: 150,
-            xtype: 'widgetcolumn',
-            dataIndex: 'mode',
-            widget: {
-                xtype: 'combo',
-                store: {
-                    type: 'option'
-                },
-                displayField: 'option',
-                valueField: 'id',
-                value: 'desc',
-                listeners: {
-                    select: function (combo, record) {
-                        var mainGrid = combo.up('gridpanel'); // Adjust the selector based on your application structure
-                        console.log(mainGrid)
-                        while (mainGrid && !mainGrid.isXType('gridpanel')) {
-                            mainGrid = mainGrid.up('gridpanel');
-                        }
+    columns: [{
+        text: 'Field Name',
+        width: 150,
+        xtype: 'widgetcolumn',
+        dataIndex: 'mode',
+        value: 'name',
+        markDirty: false,
 
-                        if (mainGrid) {
-                            var selectionModel = mainGrid.getSelectionModel();
-                            var recordIndex = mainGrid.getStore().indexOf(combo.getWidgetRecord());
-                            selectionModel.select(recordIndex);
-                            var selectedRecord = selectionModel.getSelection()[0];
-                            selectedRecord.set('mode', record.get('id'));
-                        } else {
-                            console.error('Main grid not found.');
-                        }
-                    }
-                }
+        widget: {
+            xtype: 'combo',
+            store: {
+                type: 'option'
             },
-            flex: 1
+            displayField: 'option',
+            valueField: 'id',
+            listeners: {
+                select: 'AddSelectedOptionToStore'
+            }
         },
-        { text: 'Data', dataIndex: 'data', flex: 3, sortable: false }
-    ],
-
-});
-
-
-// listeners: {
-//     select: function (combo, record) {
-//         var mainGrid = combo.up('gridpanel'); // Adjust the selector based on your application structure
-//         while (mainGrid && !mainGrid.isXType('gridpanel')) {
-//             mainGrid = mainGrid.up('gridpanel');
-//         }
-
-//         if (mainGrid) {
-//             var selectionModel = mainGrid.getSelectionModel();
-//             var recordIndex = mainGrid.getStore().indexOf(combo.getWidgetRecord());
-//             selectionModel.select(recordIndex);
-//             // var grid = combo.up('grid');
-//             // var selectionModel = grid.getSelectionModel();
-//             // var selectedRecord = selectionModel.getSelection();
-//             // console.log(selectedRecord);
-//             var selectedRecord = selectionModel.getSelection()[0];
-//             selectedRecord.set('mode', record.get('id'));
-//         } else {
-//             console.error('Main grid not found.');
-//         }
-//         // console.log(combo)
-//         // var grid = combo.getBoundList().ownerGrid; // Get the grid where the combo box is located
-//         // var selectionModel = grid.getSelectionModel();
-
-//         // var recordIndex = grid.getStore().indexOf(combo.getWidgetRecord()); // Get the index of the record associated with the combo box
-
-//         // // Select the corresponding row
-//         // selectionModel.select(recordIndex);
-//         // console.log()
-//         // var grid = combo.up('grid');
-//         // var selectionModel = grid.getSelectionModel();
-//         // var selectedRecord = selectionModel.getSelection();
-//         // console.log(selectedRecord);
-//         // selectedRecord.set('mode', record.get('id'));
-//     }
-// }/**
- * This view is an example list of people.
- */
-Ext.define('ExtractApp.view.main.List', {
-    extend: 'Ext.grid.Panel',
-    xtype: 'mainlist',
-    alias: 'view.mainlist',
-    requires: [
-        'ExtractApp.store.Personnel'
-    ],
-
-    title: 'Personnel',
-
-    store: {
-        type: 'personnel'
+        flex: 1
     },
-
-    columns: [
-        {
-            text: 'Field Name',
-            width: 150,
-            xtype: 'widgetcolumn',
-            dataIndex: 'mode',
-            widget: {
-                xtype: 'combo',
-                store: {
-                    type: 'option'
-                },
-                displayField: 'option',
-                valueField: 'id',
-                value: 'desc',
-                listeners: {
-                    select: function (combo, record) {
-                        var mainGrid = combo.up('gridpanel'); // Adjust the selector based on your application structure
-                        console.log(mainGrid)
-                        while (mainGrid && !mainGrid.isXType('gridpanel')) {
-                            mainGrid = mainGrid.up('gridpanel');
-                        }
-
-                        if (mainGrid) {
-                            var selectionModel = mainGrid.getSelectionModel();
-                            var recordIndex = mainGrid.getStore().indexOf(combo.getWidgetRecord());
-                            selectionModel.select(recordIndex);
-                            var selectedRecord = selectionModel.getSelection()[0];
-                            selectedRecord.set('mode', record.get('id'));
-                        } else {
-                            console.error('Main grid not found.');
-                        }
-                    }
-                }
-            },
-            flex: 1
-        },
-        { text: 'Data', dataIndex: 'data', flex: 3, sortable: false }
+    {
+        text: 'Data', dataIndex: 'data', flex: 3, sortable: false
+    }
     ],
-
 });
-
-
-// listeners: {
-//     select: function (combo, record) {
-//         var mainGrid = combo.up('gridpanel'); // Adjust the selector based on your application structure
-//         while (mainGrid && !mainGrid.isXType('gridpanel')) {
-//             mainGrid = mainGrid.up('gridpanel');
-//         }
-
-//         if (mainGrid) {
-//             var selectionModel = mainGrid.getSelectionModel();
-//             var recordIndex = mainGrid.getStore().indexOf(combo.getWidgetRecord());
-//             selectionModel.select(recordIndex);
-//             // var grid = combo.up('grid');
-//             // var selectionModel = grid.getSelectionModel();
-//             // var selectedRecord = selectionModel.getSelection();
-//             // console.log(selectedRecord);
-//             var selectedRecord = selectionModel.getSelection()[0];
-//             selectedRecord.set('mode', record.get('id'));
-//         } else {
-//             console.error('Main grid not found.');
-//         }
-//         // console.log(combo)
-//         // var grid = combo.getBoundList().ownerGrid; // Get the grid where the combo box is located
-//         // var selectionModel = grid.getSelectionModel();
-
-//         // var recordIndex = grid.getStore().indexOf(combo.getWidgetRecord()); // Get the index of the record associated with the combo box
-
-//         // // Select the corresponding row
-//         // selectionModel.select(recordIndex);
-//         // console.log()
-//         // var grid = combo.up('grid');
-//         // var selectionModel = grid.getSelectionModel();
-//         // var selectedRecord = selectionModel.getSelection();
-//         // console.log(selectedRecord);
-//         // selectedRecord.set('mode', record.get('id'));
-//     }
-// }
