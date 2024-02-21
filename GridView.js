435e454001f8ea84553f3a15f6e1ab052c024a42
blackdayalpha <93610550+blackdayalpha@users.noreply.github.com>
2024-02-21 14:26:12 +0530

    Update GridView.js

--- GridView.js
+++ GridView.js
@@ -3,40 +3,41 @@ Ext.define('ExtractApp.view.main.GridView', {
     xtype: 'app-gridView',
     alias: 'view.gridView',
     id: 'id-gridview',
-
-    // controller: 'main',
-    // viewModel: 'main',
+    reference: 'id-gridview',
     autoscroll: true,
+    maxWidth: 800,
+    frame: true,
+    title: 'Select Fields',
+
+    margin: "10 0 0 0",
+    padding: '25 25 25 25',
 
     items: [{
         id: 'gridView',
         xtype: 'mainlist',
-        height: 400,
+        height: 450,
         autoscroll: true,
     },
     {
-        layout: 'hbox',
+        layout: {
+            type: 'hbox',
+            align: 'middle',
+            pack: 'start'
+        },
+        margin: '25 0 0 0',
         items: [{
             xtype: 'button',
             id: 'cancel-btn',
             text: 'Cancel',
+            tooltip: 'Remove the Grid',
             handler: 'CancelJsonGeneration',
         }, {
             xtype: 'button',
             id: 'genBtn',
             text: 'Generate JSON',
-            handler: 'generateJson',
+            tooltip: 'Generate and View JSON Data',
+            handler: 'GenerateJsonHandle',
         }]
     },
-
-
-        // {
-        //     title: 'Home',
-        //     iconCls: 'fa-home',
-        //     // The following grid shares a store with the classic version's grid as well!
-        //     items: [{
-        //         xtype: 'mainlist'
-        //     }]
-        // }
     ]
 });
